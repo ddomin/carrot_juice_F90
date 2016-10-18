ed50eaf8953c81a49bbd4c6002a7831c6d862fde
Matthieu Haefele <matthieu.haefele@maisondelasimulation.fr>
2016-10-18 17:58:36 +0200

    Changing file names

--- growing/growing.h
+++
@@ -1,4 +0,0 @@
-
-void plant(int* field, int size, int seed);
-void grow(int* field, int size, int growing_factor);
-void harvest(int* field,int* carrots, int size);
--- pressing/pressing.h
+++
@@ -1,2 +0,0 @@
-
-float press(int* carrots, int size, float pressing_factor);
